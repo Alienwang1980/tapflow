6ba2c1f3953093b00ee1eb9fae0117308d7ffccc
Hermes <hermes@nousresearch.com>
2026-08-25 21:22:11 +0800

    chore: bump version to 1.0.9

--- setup.py
+++ setup.py
@@ -68,8 +68,8 @@ OPTIONS = {
         "CFBundleName": "Tapflow",
         "CFBundleDisplayName": "Tapflow",
         "CFBundleIdentifier": "com.tapflow.app",
-        "CFBundleVersion": "1.0.8",
-        "CFBundleShortVersionString": "1.0.8",
+        "CFBundleVersion": "1.0.9",
+        "CFBundleShortVersionString": "1.0.9",
         "NSHighResolutionCapable": True,
         "LSUIElement": True,
         "NSAppleEventsUsageDescription": "Tapflow needs Accessibility access to simulate keyboard input.",
@@ -88,7 +88,7 @@ OPTIONS = {
 
 setup(
     name="Tapflow",
-    version="1.0.8",
+    version="1.0.9",
     app=APP,
     data_files=DATA_FILES,
     options={"py2app": OPTIONS},
